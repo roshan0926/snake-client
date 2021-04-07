82567ea578d66a5f6546ba327e0120bf07b2124d
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-07 16:46:01 +0000

    added on screen name

--- client.js
+++ client.js
@@ -4,6 +4,12 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
+  
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: RNA');
+    return;
+  });
   conn.on('data', (message) => {console.log('you ded cuz you idled')})
   
   // interpret incoming data as text
@@ -12,3 +18,9 @@ const connect = function() {
   return conn;
 }
 module.exports = connect;
+
+
+// server.on('connection', (client) => {
+//   console.log('New client connected!');
+//   client.write('Hello there!');
+// });
