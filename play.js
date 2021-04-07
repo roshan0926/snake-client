c4220963c81f429dca0e3fae0d39447fb203954d
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-07 17:19:33 +0000

    added input file and exported to play.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const connect = require('./client');
+const setupInput = require('./input');
+
 console.log('Connecting ...');
 connect();
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-setupInput()
 
-process.stdin.on('data', (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-});
-
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-handleUserInput()
+setupInput();
