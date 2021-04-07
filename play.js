3f97553f6e0911ddf61219bc66e6c7219e45cb3f
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-07 16:29:51 +0000

    updated ip and host

--- play.js
+++ play.js
@@ -5,8 +5,8 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: '135.23.222.131',
+    port: 50542
   });
   conn.on('data', (message) => {console.log('you ded cuz you idled')})
   
